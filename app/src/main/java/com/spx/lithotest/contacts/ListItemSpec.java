c9788257362696f9ecb76319b57c108acd827083
shaopengxiang <shaopengxiang@sogou-inc>
2018-06-22 19:42:50 +0800

    add feed list activity

--- app/src/main/java/com/spx/lithotest/contacts/ListItemSpec.java
+++ app/src/main/java/com/spx/lithotest/contacts/ListItemSpec.java
@@ -4,20 +4,16 @@ package com.spx.lithotest.contacts;
 import android.graphics.Color;
 import android.view.View;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.litho.ClickEvent;
 import com.facebook.litho.Column;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
-import com.facebook.litho.ComponentLayout;
 import com.facebook.litho.Row;
 import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
 import com.facebook.litho.annotations.OnEvent;
 import com.facebook.litho.annotations.Prop;
-import com.facebook.litho.fresco.FrescoImage;
 import com.facebook.litho.widget.Text;
 import com.facebook.yoga.YogaEdge;
 import com.spx.lithotest.R;
@@ -50,25 +46,27 @@ public class ListItemSpec {
                 )
                 .build();
 
-        final DraweeController controller = Fresco.newDraweeControllerBuilder()
-                .setUri(photo)
-                .build();
+        return null;
 
-        return Row.create(c)
-                .backgroundAttr(android.R.attr.selectableItemBackground)
-                .paddingDip(YogaEdge.ALL, 16)
-                .child(
-                        FrescoImage.create(c)
-                                .controller(controller)
-                                .failureImageRes(R.drawable.ic_account_box_black_48dp)
-                                .placeholderImageRes(R.drawable.ic_account_box_black_48dp)
-                                .backgroundColor(Color.GRAY)
-                                .widthDip(80)
-                                .heightDip(80)
-                )
-                .child(column)
-                .clickHandler(ListItem.onClick(c))
-                .build();
+//        final DraweeController controller = Fresco.newDraweeControllerBuilder()
+//                .setUri(photo)
+//                .build();
+//
+//        return Row.create(c)
+//                .backgroundAttr(android.R.attr.selectableItemBackground)
+//                .paddingDip(YogaEdge.ALL, 16)
+//                .child(
+//                        FrescoImage.create(c)
+//                                .controller(controller)
+//                                .failureImageRes(R.drawable.ic_account_box_black_48dp)
+//                                .placeholderImageRes(R.drawable.ic_account_box_black_48dp)
+//                                .backgroundColor(Color.GRAY)
+//                                .widthDip(80)
+//                                .heightDip(80)
+//                )
+//                .child(column)
+//                .clickHandler(ListItem.onClick(c))
+//                .build();
     }
 
     @OnEvent(ClickEvent.class)
